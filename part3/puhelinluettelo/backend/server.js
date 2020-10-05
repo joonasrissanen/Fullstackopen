95a1d11483f66c4fd21abe6f000f41ff228a2c71
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-10-05 11:05:09 +0300

    add gitignore

---
+++ part3/puhelinluettelo/backend/server.js
@@ -0,0 +1,86 @@
+let persons = [
+  {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    }
+];
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const jsonParser = bodyParser.json();
+
+app.use(morgan('tiny'));
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status - :response-time ms :body'));
+app.use(cors());
+
+app.get('/info', (req, res) => {
+  const currentDate = new Date();
+  const responseBody = `
+    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>${currentDate.toString()}</p>
+  `;
+  res.send(responseBody);
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
+});
+
+app.post('/api/persons', jsonParser, (req, res) => {
+  const person = req.body;
+  if (!person || !person.name || !person.number) {
+    const error = { error: 'name and number must be defined' };
+    res.status(400).json(error);
+  } else if (persons.map(p => p.name).includes(person.name)) {
+    const error = { error: 'name must be unique' };
+    res.status(400).json(error);
+  } else {
+    const newId = Math.floor(Math.random() * Math.max(100000));
+    const newPerson = {
+      ...person,
+      id: newId,
+    };
+    persons.push(newPerson);
+    res.sendStatus(201);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
