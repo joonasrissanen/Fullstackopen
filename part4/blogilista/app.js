016a7ced23ca1c6fd184e4fa9ac1cb684130140f
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-10-16 15:58:55 +0300

    add 4.1-4.2

---
+++ part4/blogilista/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+
+const app = express();
+const mongoUrl = config.MONGODB_URI;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
---
+++ part4/blogilista/utils/config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+let PORT = process.env.PORT || 3003;
+let MONGODB_URI = process.env.MONGODB_URI;
+
+module.exports = {
+    MONGODB_URI,
+    PORT,
+};
