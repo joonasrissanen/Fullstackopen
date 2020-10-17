63a764f437d4cb38612c7cadefa0c5b635e9653d
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-10-17 17:38:12 +0300

    add 4.15-4.17

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -3,9 +3,10 @@ require('express-async-errors');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRoutes = require('./controllers/users');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
+const usersRouter = require('./controllers/users');
 
 const app = express();
 const mongoUrl = config.MONGODB_URI;
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,16 +1,25 @@
 const blogsRouter = require('express').Router();
 require('express-async-errors');
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs.map(blog => blog.toJSON()));
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result.toJSON());
+  const body = request.body;
+  const user = await User.findById(body.userId);
+  console.log('Body user id', body.userId, 'Mongo user id', user._id);
+  const blog = new Blog({
+    ...body,
+    user: user._id
+  });
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+  response.status(201).json(savedBlog.toJSON());
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -22,5 +31,5 @@ blogsRouter.put('/:id', async (request, response) => {
   const body = request.body;
   const result = await Blog.findByIdAndUpdate(request.params.id, body, { new: true });
   response.json(result.toJSON());
-})
+});
 module.exports = blogsRouter;
