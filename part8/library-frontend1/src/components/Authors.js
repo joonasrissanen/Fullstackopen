2a4035c6561b51ac03a5d59bb84784601992c027
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-12-23 12:21:21 +0200

    fix

---
+++ part8/library-frontend1/src/components/Authors.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react'
+import { gql, useQuery, useMutation } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`
+
+const EDIT_BORN = gql`
+  mutation editBorn($name: String!, $born: Int!) {
+    editBorn(name: $name, born: $born) {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+  const [selectedAuthor, setAuthor] = useState(null)
+  const [authors, setAuthors] = useState([])
+  const [year, setYear] = useState('')
+  const [error, setError] = useState('');
+  const [ changeBorn, resultBorn ] = useMutation(EDIT_BORN)
+  useEffect(() => {
+    if (resultBorn.data && resultBorn.data.editBorn === null) {
+      setError('Author not found')
+    }
+  }, [resultBorn.data])
+  useEffect(() => {
+    if (result.data && result.data.allAuthors) {
+      setAuthors(result.data.allAuthors);
+      setAuthor(result.data.allAuthors[0].name)
+    }
+  }, [result.data])
+  if (!props.show) {
+    return null
+  }
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  const updateAuthor = (e) => {
+    e.preventDefault()
+    changeBorn({ variables: { name: selectedAuthor, born: Number(year) }})
+    setYear('')
+  }
+  return (
+    <div>
+      <h2>authors</h2>
+      {error && <div>{error}</div>}
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <form onSubmit={updateAuthor}>
+        <select onChange={e => setAuthor(e.target.value)}>
+          {authors.map(author => <option key={author.id} value={author.name}>{author.name}</option>)}
+        </select>
+        <div>
+          born <input value={year} onChange={e => setYear(e.target.value)} />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
