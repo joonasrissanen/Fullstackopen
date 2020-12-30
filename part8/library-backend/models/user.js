fd5931e4e90a03a2ce21f0f2ef1c85242fcf3c1c
Joonas Rissanen <joonas.rissanen@cgi.com>
2020-12-30 15:17:27 +0200

    add 8.13-8.16

---
+++ part8/library-backend/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  favoriteGenre: {
+    type: String
+  },
+  passwordHash: {
+    type: String,
+  }
+})
+
+schema.plugin(uniqueValidator)
+
+schema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  }
+});
+
+module.exports = mongoose.model('User', schema)
